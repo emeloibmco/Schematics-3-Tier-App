5026363a1b3ac4b77cf129761cb661f1b90ca0f2
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-04-03 09:43:38 -0500

    Update files new

--- Terraform/vm.tf
+++ Terraform/vm.tf
@@ -3,15 +3,6 @@ provider "ibm" {
   region             = "us-south"
 }
 
-variable "ibmcloud_api_key" {
-  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
-}
-
-variable "resource_group" {
-  description = "Name of resource group to provision resources"
-  default     = "landing-zone"
-}
-
 data "ibm_resource_group" "group" {
   name = "${var.resource_group}"
 }
