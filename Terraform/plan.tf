b59d1eedbcae7cf2e27c5984874c30d8ab808feb
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-06-28 22:55:40 -0500

    update template

--- Terraform/plan.tf
+++ Terraform/plan.tf
@@ -106,6 +106,7 @@ output sshcommand {
 
 data "ibm_container_cluster_config" "iks_cluster_config" {
     cluster_name_id = "iks-joomla"
+    admin           = true
     resource_group_id = "${data.ibm_resource_group.group.id}"
     depends_on = ["ibm_container_vpc_cluster.iks-joomla"]
 }
@@ -113,7 +114,8 @@ data "ibm_container_cluster_config" "iks_cluster_config" {
 provider "kubernetes" { 
     load_config_file       = "false"
     host                   = "${data.ibm_container_cluster_config.iks_cluster_config.host}"
-    token                  = "${data.ibm_container_cluster_config.iks_cluster_config.token}"
+    client_certificate     = "${data.ibm_container_cluster_config.iks_cluster_config.admin_certificate}"
+    client_key             = "${data.ibm_container_cluster_config.iks_cluster_config.admin_key}"
     cluster_ca_certificate = "${data.ibm_container_cluster_config.iks_cluster_config.ca_certificate}"
 }
 
