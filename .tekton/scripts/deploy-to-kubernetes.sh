ae8111267dc076e593b8fda25ca65d4705850be8
Jesús Orlando Montoya Mejía <jesus.orlando.montoya.mejia@ibm.com>
2020-07-02 14:38:52 -0500

    Tekton..

--- .tekton/scripts/deploy-to-kubernetes.sh
+++ .tekton/scripts/deploy-to-kubernetes.sh
@@ -47,7 +47,7 @@ LB_NAME=hw-lb-svc
 if kubectl get svc -n ${CLUSTER_NAMESPACE} | grep ${LB_NAME}; then
   echo -e "LoadBalancer service with ${LB_NAME} already exists"
   echo "Deleting ${LB_NAME} service"
-  kubectl delete -n ${CLUSTER_NAMESPACE} service hw-lb-svc
+  kubectl delete -n ${CLUSTER_NAMESPACE} service ${LB_NAME}
 fi
 kubectl expose deployment/joomla --type=LoadBalancer --name=${LB_NAME} --port=80 --target-port=80 --namespace ${CLUSTER_NAMESPACE}
 
