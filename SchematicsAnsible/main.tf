7186a415c8dd3790514088786574448a88d50626
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-07-08 17:33:07 -0500

    upload files

--- SchematicsAnsible/main.tf
+++ SchematicsAnsible/main.tf
@@ -118,3 +118,35 @@ module "accesscheck" {
   bastion_host    = module.bastion.bastion_ip_addresses[0]
   target_hosts    = concat(module.frontend.primary_ipv4_address, module.backend.primary_ipv4_address)
 }
+
+
+##################################################################################################
+#  Config servers
+##################################################################################################
+
+resource "null_resource" "ansible" {
+  connection {
+    bastion_host = module.bastion.bastion_ip_addresses[0]
+    host         = "0.0.0.0"
+    #private_key = "${file("~/.ssh/ansible")}"
+    private_key = var.ssh_private_key
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "ansible" {
+    plays {
+      playbook {
+        file_path = "${path.module}/ansible-data/mysqlvsi.yml"
+      }
+      inventory_file = "${path.module}/terraform_inv.py"
+      verbose        = true
+    }
+    ansible_ssh_settings {
+      insecure_no_strict_host_key_checking = true
+      connect_timeout_seconds              = 60
+    }
+  }
+  depends_on = [local_file.terraform_source_state]
+}
